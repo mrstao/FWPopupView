99ce14fc24ecc1951ddffa1c0a73ed32033861f2
Stao <906870651@qq.com>
2025-08-30 14:29:49 +0800

    支持SMP

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "FWPopupView",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "FWPopupView",
+            targets: ["FWPopupView"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "FWPopupView",
+            dependencies: ["SnapKit"],
+            path: "FWPopupView",
+            resources: [
+                .process("Resources/FWPopupView.bundle")
+            ],
+            publicHeadersPath: ""
+        )
+    ]
+)
